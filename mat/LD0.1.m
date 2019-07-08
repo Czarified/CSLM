56f4e774b8d06a48f5ea9e4ea4c08c9f77bf3268
Crews <bcrews@txt.textron.com>
2019-07-08 18:19:37 -0500

    LD0.1 Work
    Added property, applied property, modified some loads application. Not currently solving.

--- mat/LD0.1.m
+++ mat/LD0.1.m
@@ -15,5 +15,12 @@ $       For allowables, the lower value between L and LT directions is given,
 $       and the values are for a temperature of 70deg F. The tensile ultimate
 $       value is given, and the yield for compression and shear.
 $       All data in US units, as standard for CSLM LD0 (lb, in, psi)
-MAT1    301     29.0E+6 11.2E+6 0.27    0.29     8E-6   100
+MAT1    301     29.0E+6 11.2E+6 0.27    0.29     8E-6   100.
         73E+3   23E+3   50E+3
+        
+        
+
+$ Property Record : pshell.301.5
+$ Description of Material :
+$       Shell of 5mm thickness made of m_301SS.
+PSHELL  3015    301     0.005
